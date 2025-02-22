8406c80028fcd635c14f4df0d8197cfe9aca3405
teodorbarbu <teodorbarbu@yahoo.com>
2025-02-21 22:35:42 -0500

    practice
    Test JavaScript Challenges.

--- test.test.js
+++ test.test.js
@@ -1,11 +1,15 @@
-function reverseInt(n) {
-  const reversed = n.toString().split("").reverse().join("");
-  return parseInt(reversed) * Math.sign(n);
+function removeDuplicates1(arr) {
+  const uniqueArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArr.includes(arr[i])) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+  return uniqueArr;
 }
-
-console.log(reverseInt(0));
-
-console.log(reverseInt(5));
-console.log(reverseInt(15));
-
-console.log(reverseInt(-2359));
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
+console.log(removeDuplicates([1, 2, 3, 2, 4, 1, 5]));
+console.log(removeDuplicates(["apple", "banana", "orange", "banana", "kiwi"]));
+console.log(removeDuplicates([true, true, false, true, false]));

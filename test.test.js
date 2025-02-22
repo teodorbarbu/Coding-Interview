af1c4f04ee7cb2f66618c13e50dd2f05e2f7cadf
teodorbarbu <teodorbarbu@yahoo.com>
2025-02-21 22:11:10 -0500

    practice
    Test JavaScript Challenges.

--- test.test.js
+++ test.test.js
@@ -1,21 +1,11 @@
-function countVowels(str) {
-  const formattedStr = str.toLowerCase();
-  let count = 0;
-  for (let i = 0; i < formattedStr.length; i++) {
-    const char = formattedStr[i];
-    if (
-      char === "a" ||
-      char === "e" ||
-      char === "i" ||
-      char === "o" ||
-      char === "u"
-    ) {
-      count++;
-    }
-  }
-  return count;
+function reverseInt(n) {
+  const reversed = n.toString().split("").reverse().join("");
+  return parseInt(reversed) * Math.sign(n);
 }
-console.log(countVowels("Hello, World!"));
-console.log(countVowels("JavaScript"));
-console.log(countVowels("Coding Challenge"));
-console.log(countVowels("OpenAI Chatbot"));
+
+console.log(reverseInt(0));
+
+console.log(reverseInt(5));
+console.log(reverseInt(15));
+
+console.log(reverseInt(-2359));

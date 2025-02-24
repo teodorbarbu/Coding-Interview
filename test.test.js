1037f1dee714063459217f4c7a56dd05fa2da6f6
teodorbarbu <teodorbarbu@yahoo.com>
2025-02-24 13:56:01 -0500

    practice.
    Test JavaScript Challenges.

--- test.test.js
+++ test.test.js
@@ -1,26 +1,56 @@
-function intersectionArray1(arr1, arr2) {
-  const intersectionArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr2.includes(arr1[i])) {
-      intersectionArr.push(arr1[i]);
+function chunk1(array, size) {
+  const chunked = [];
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
     }
   }
-  return intersectionArr;
+  return chunked;
 }
 
-function intersectionArray(arr1, arr2) {
-  const set = new Set(arr1);
-  const intersectionArr = [];
-  for (let num of arr2) {
-    if (set.has(num)) {
-      intersectionArr.push(num);
+function chunk2(array, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+
+function chunk3(arr, size) {
+  const res = [];
+  let subarr = [];
+  for (let i = 0; i < arr.length; i++) {
+    subarr.push(arr[i]);
+    if (subarr.length === size) {
+      res.push(subarr);
+      subarr = [];
     }
   }
-  return intersectionArr;
+  if (subarr.length) {
+    res.push(subarr);
+  }
+  return res;
 }
 
-console.log(intersectionArray([1, 2, 3, 4, 5], [3, 4, 5, 6, 7])); //[ 3, 4, 5 ]
+function chunk(arr, size) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size));
+  }
+  return res;
+}
 
-console.log(intersectionArray([10, 20, 30], [30, 40, 50])); //[ 30 ]
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
+// [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
+console.log(chunk([1, 2, 3], 1));
+// [[1], [2], [3]]
+console.log(chunk([1, 2, 3, 4, 5], 3));
+// [[1, 2, 3], [4, 5]]
 
-console.log(intersectionArray([1, 2, 3], [4, 5, 6])); //[]
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5));
+//([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]])

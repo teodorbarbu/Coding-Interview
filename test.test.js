6c4dd545aa5c956f9023d43103723d9daa7f435e
teodorbarbu <teodorbarbu@yahoo.com>
2025-03-05 21:53:38 -0500

    practice.
    Test JavaScript Challenges.

--- test.test.js
+++ test.test.js
@@ -1,32 +1,12 @@
-function anagrams1(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
+function findMissingLetter(arr) {
+  const alphabeth = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const startIndex = alphabeth.indexOf(arr[0]);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== alphabeth[startIndex + i]) {
+      return alphabeth[startIndex + i];
     }
   }
-  return true;
-}
-function buildCharMap(str) {
-  const charMap = {};
-  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-  return charMap;
+  return [];
 }
 
-function anagrams(stringA, stringB) {
-  return cleanString(stringA) === cleanString(stringB);
-}
-function cleanString(str) {
-  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
-}
-
-console.log(anagrams("hello", "llohe"));
-console.log(anagrams("Whoa! Hi!", "Hi! Whoa!"));
-console.log(anagrams("A tree, a life, a bench", "A tree, a fence, a yard"));
+console.log(findMissingLetter("a", "b", "c", "d", "f"));
